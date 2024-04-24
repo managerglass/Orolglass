0c79e16d406535330775fa8be2de5cbcc0b7fba0
managerglass <ti@managerglass.com.br>
2024-04-24 09:25:50 -0300

    adicionando urls root para media

--- backend/urls.py
+++ backend/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -38,4 +40,4 @@ urlpatterns = [
         'api/v1/',
         include('backend.destaque.urls')
     )
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
