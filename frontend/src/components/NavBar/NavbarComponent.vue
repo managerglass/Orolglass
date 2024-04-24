7c8f8b8d57f896f3119a4fb60206cfcf855130c7
joaolucasgomess <jlnapole12@gmail.com>
2024-04-24 16:04:55 -0300

    NavBar

--- frontend/src/components/NavBar/NavbarComponent.vue
+++ frontend/src/components/NavBar/NavbarComponent.vue
@@ -1,6 +1,39 @@
+
+import { useLink } from 'vue-router';
+
+import { useLink } from 'vue-router';
+
+import { useLink } from 'vue-router';
+
 <template>
-
-
+    <div class="header">
+        <div class="frame1">
+            <div class="logo">
+                <img class="w-40" src="../../assets/logo.svg" alt="Logo" />
+            </div>
+            <div class="frame1-dados">
+                <button class="flex" @click.prevent="openLink('https://api.whatsapp.com/send?phone=55032999362017')">
+                    <div>
+                        <i class="fa-solid fa-phone text-2xl px-2"></i>
+                    </div>
+                    <div class="hidden md:block">
+                        <p>(32) 99936 2017</p>
+                    </div>
+                </button>
+                <button class="flex" @click.prevent="openLink('https://maps.app.goo.gl/hyGo1kPEPPxDJbmS6')">
+                    <div>
+                        <i class="fa-solid fa-location-dot text-2xl px-2"></i>
+                    </div>
+                    <div class="hidden md:block">
+                        <p>Av. Dante Bruno, 335 - Dornelas/Muriaé-MG</p>
+                    </div>
+                </button>
+            </div>
+        </div>
+        <div class="frame2-botoes">
+            <router-link v-for="link in links" :key="link.name" :to="link.path">{{ link.name }}</router-link>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,11 +43,41 @@ export default {
 
     data() {
         return {
+            links: [
+                { name: 'Home', path: '/' },
+                { name: 'Empresa', path: '/sobre' },
+                { name: 'Obras', path: '/obras' },
+                { name: 'Eventos', path: '/eventos' },
+                { name: 'Contatos', path: '/contatos' }
+            ]
+        }
+    },
 
+    methods: {
+        openLink(link) {
+            window.open(link, '_blank')
         }
     }
 }
 
 </script>
 
-<style></style>
+<style scoped lang="scss">
+.header {
+    @apply w-full px-6 md:px-24 divide-y-2 fixed top-0
+}
+
+.frame1 {
+    @apply flex w-full justify-between py-8
+}
+
+.frame1-dados {
+    @apply flex gap-8
+}
+.frame2 {
+    @apply py-4
+}
+.frame2-botoes {
+    @apply flex w-full justify-around py-5 md:text-xl
+}
+</style>
