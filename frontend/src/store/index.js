6c84d5764496578606b102132d68b5437230764f
joaolucasgomess <jlnapole12@gmail.com>
2024-04-25 10:11:49 -0300

    add store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,13 +1,23 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    BASE_URL: 'http://192.168.18.106:8000/api/v1/'
   },
   getters: {
   },
   mutations: {
   },
   actions: {
+    async getImages({ commit }) {
+      try {
+        const response = await axios.get(`${this.state.BASE_URL}imagem?tipo=DESTAQUE`)
+        return response.data
+      } catch (err) {
+        console.error(err)
+      }
+    }
   },
   modules: {
   }
